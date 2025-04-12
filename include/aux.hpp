6c9b71dbe8cb785ce9c8e5c760c0e8145cc05b0a
DashyDaFur <DashyYTUwU@outlook.com>
2025-04-12 00:03:26 -0700

    Add files via upload

--- include/aux.hpp
+++ include/aux.hpp
@@ -1,1 +1,12 @@
+#ifndef AUX_HPP_INCLUDED
+#define AUX_HPP_INCLUDED
 
+#include <vector>
+#include <string>
+
+void Pause();
+int aleatorio(int inicio, int fin);
+double* GenerarPolinomioAleatorio(int grado);
+void ExportarCSV(const std::vector<std::vector<double>>& datos, const std::string& nombre);
+
+#endif // AUX_HPP_INCLUDED
