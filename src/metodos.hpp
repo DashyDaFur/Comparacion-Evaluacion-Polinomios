e124395d9ece19f0f27cb8775c46f147a6006c9f
DashyDaFur <DashyYTUwU@outlook.com>
2025-04-12 00:03:57 -0700

    Delete src/metodos.hpp

--- src/metodos.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef METODOS_HPP_INCLUDED
-#define METODOS_HPP_INCLUDED
-
-double EvaluarPolinomioEstandar(double* a, int grado, double n);
-double EvaluarPolinomioHorner(double* a, int grado, double n);
-
-#endif // METODOS_HPP_INCLUDED
